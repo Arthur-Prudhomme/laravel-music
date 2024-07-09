45250554403f4265591f21f978052baf56663ed4
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2024-07-09 09:53:02 +0200

    added track component

---
+++ resources/js/Components/Track.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="bg-slate-500 w-fit p-2 m-1 rounded-lg">
+		<img :src="image" alt="track img" />
+		<p>
+			<b>{{ title }}</b> - ({{ artist }})
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Track",
+	props: ["title", "artist", "image"],
+};
+</script>
--- resources/js/Pages/Track/index.vue
+++ resources/js/Pages/Track/index.vue
@@ -4,22 +4,26 @@
 			<div>
 				<h1>Liste de mes tracks</h1>
 				<br />
-				<ul>
-					<li v-for="track in tracks" :key="track.uuid">
-						{{ track.title }} ({{ track.artist }})
-					</li>
-				</ul>
+				<Track
+					v-for="track in tracks"
+					:key="track.uuid"
+					:title="track.title"
+					:artist="track.artist"
+					:image="track.image"
+				/>
 			</div>
 		</template>
 	</MusicLayout>
 </template>
 
 <script>
+import Track from "@/Components/Track.vue";
 import MusicLayout from "@/Layouts/MusicLayout.vue";
 export default {
 	name: "Index",
 	components: {
 		MusicLayout,
+		Track,
 	},
 	props: { tracks: Array },
 };
